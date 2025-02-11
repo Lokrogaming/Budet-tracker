23f2315e0fb358092758ad670e3110353d332414
Lokrogamer <lokrogamer.29@gmail.com>
2025-02-11 10:06:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('loadingScreen').style.display = 'none';
         document.getElementById('content').style.display = 'block';
-    }, };// Simulierte Ladezeit
+    }, );// Simulierte Ladezeit
 
     // Wenn das Budget bereits gesetzt wurde, den Bereich anzeigen
     checkBudget();
